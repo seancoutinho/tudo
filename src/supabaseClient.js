551b07e3c6aeefdf811e1d2675d4094054bb6e52
Sean Coutinho <SeanCoutinho@my.uopeople.edu>
2024-06-26 16:13:10 +0200

    fix

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://gugwbfwknkponvvnencn.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1Z3diZndrbmtwb252dm5lbmNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkzOTg3NTcsImV4cCI6MjAzNDk3NDc1N30.pI1tB7FiEakj_QynIFfhxln7qcEFIMUhlM5Ukvo6CGA'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
